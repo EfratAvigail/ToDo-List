5e61c5df2e81f878dfa5cd73037a81fc2927c6a3
EfratAvigail <f0548573828@gmail.com>
2025-02-09 01:07:47 +0200

    API

--- render-app/server.js
+++ render-app/server.js
@@ -1,16 +1,15 @@
-
-
 const express = require('express');
-const axios = require('axios'); // נשתמש ב-Axios כדי לקרוא ל-API של Render
+const axios = require('axios');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Endpoint של GET
+// Endpoint שמחזיר את רשימת האפליקציות
 app.get('/apps', async (req, res) => {
     try {
         const response = await axios.get('https://api.render.com/v1/services', {
             headers: {
-                'Authorization': "rnd_BUpWyx2rhSToma0CErTw1zTEC0rs" // החלף ב-API Key שלך
+                'Authorization': `rnd_BUpWyx2rhSToma0CErTw1zTEC0rs` // החלף ב-API Key שלך
             }
         });
         res.json(response.data);
